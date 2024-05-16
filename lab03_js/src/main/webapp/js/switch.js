c9c1b988ebb81e9106c201c516e3816cf8839a84
sveiin <veiin9230@gmail.com>
2024-05-16 18:11:23 +0900

    switch

---
+++ lab03_js/src/main/webapp/js/switch.js
@@ -0,0 +1,31 @@
+/**
+ * switch.html에 포함
+ */
+
+// console.log('test');
+
+const weekday = document.getElementById('weekday');
+const btn = document.getElementById('btn');
+const result = document.getElementById('result');
+
+function selectListener() {
+    const value = weekday.value;
+    switch (value) {
+        case 'mon':
+        case 'tue':
+        case 'wed':
+        case 'thu':
+        case 'fri':
+            result.innerHTML = '학원 출석';
+            break;
+        case 'sat':
+        case 'sun':
+            result.innerHTML = '쉼';
+            break;
+        default:
+            result.innerHTML = '모름';
+            
+    }
+}
+
+btn.addEventListener('click', selectListener);
