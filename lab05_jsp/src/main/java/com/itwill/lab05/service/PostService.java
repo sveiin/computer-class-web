491ee8c54da621b211c442871662835b5d1dbb22
sveiin <veiin9230@gmail.com>
2024-05-30 20:11:45 +0900

    post delete, update

--- lab05_jsp/src/main/java/com/itwill/lab05/service/PostService.java
+++ lab05_jsp/src/main/java/com/itwill/lab05/service/PostService.java
@@ -48,4 +48,24 @@ public enum PostService {
     	return post; // 검색한 Post 객체를 컨트롤러에게 리턴
     	
     }
+    
+    public int delete(int id) {
+    	log.debug("delete(id={})", id);
+    	
+    	int result = postDao.delete(id);
+    	log.debug("delete result = {}", result);
+    	
+    	return result;
+    }
+    
+    public int update(Post post) {
+    	log.debug("update({})", post);
+    	
+    	// 영속성 계층의 메서드를 호출해서 DB posts 테이블을 update
+    	int result = postDao.update(post);
+    	log.debug("update result = {}", result);
+    	
+    	return result;
+    }
+    
 }
